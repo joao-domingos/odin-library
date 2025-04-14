f5e8620aaff5f94c9da28a8e28c14322a8486f03
joao-domingos <joaopedro.mdomingos@gmail.com>
2025-04-14 20:02:36 -0300

    showBooks working with tables

--- objectConstructor.js
+++ objectConstructor.js
@@ -5,9 +5,10 @@ function Book(id, title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = function() {
-        if (read == "yes") return "aleadry read"
-        else return "didnt read"
+    this.read = read;
+
+    this.getReadStatus = function() {
+        return this.read === "yes" ? "already read" : "didn't read";
     }
 
     function info() {
@@ -23,10 +24,58 @@ function addBookToLibrary(title, author, pages, read) {
 
 //modificar depois
 function showBooks() {
-    for (let book of myLibrary) {
-        console.log(book.id, book.title);
-    }
+    generateTable();
 }
+
+function generateTable() {
+    // creates a <table> element and a <tbody> element
+    const tbl = document.createElement("table");
+    const tblBody = document.createElement("tbody");
+
+    // table header
+    const headerRow = document.createElement("tr");
+    ["title", "author", "pages", "read", "id"].forEach(headerText => {
+        const th = document.createElement("th");
+        th.textContent = headerText;
+        headerRow.appendChild(th);
+    });
+    tblBody.appendChild(headerRow);
+  
+  // for each book create all cell info 1 by 1
+  for (let i = 0; i < myLibrary.length; i++) {
+    const book = myLibrary[i];
+    const row = document.createElement("tr");
+
+    const titleCell = document.createElement("td");
+    titleCell.textContent = book.title;
+    row.appendChild(titleCell);
+
+    const authorCell = document.createElement("td");
+    authorCell.textContent = book.author;
+    row.appendChild(authorCell);
+
+    const pagesCell = document.createElement("td");
+    pagesCell.textContent = book.pages;
+    row.appendChild(pagesCell);
+
+    const readCell = document.createElement("td");
+    readCell.textContent = book.getReadStatus();
+    row.appendChild(readCell);
+
+    const idCell = document.createElement("td");
+    idCell.textContent = book.id;
+    row.appendChild(idCell);
+
+    tblBody.appendChild(row);
+  }
+  
+    // put the <tbody> in the <table>
+    tbl.appendChild(tblBody);
+    // appends <table> into <body>
+    document.body.appendChild(tbl);
+    // sets the border attribute of tbl to '2'
+    tbl.setAttribute("border", "2");
+  }
 
 addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 310, "yes");
 addBookToLibrary("1984", "George Orwell", 328, "no");
